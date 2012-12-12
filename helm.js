48786fc42e4c5187c410772a3d5505b9d43150c0
Juan La Jara <juanlajara@gmail.com>
2012-12-11 20:17:45 -0500

    added jshint fixes

--- helm.js
+++ helm.js
@@ -14,56 +14,64 @@
 
 // Add the Curry as learned from Angus Croll: http://javascriptweblog.wordpress.com/2010/04/05/curry-cooking-up-tastier-functions/
 
-if( typeof toArray !== 'function') {
-	function toArray(leEnum) {
-		return Array.prototype.slice.call(leEnum);
-	}
-}
+/*jshint smarttabs:true, forin:true, noarg:true, noempty:true, eqeqeq:true, laxbreak:true, bitwise:true, strict:true, undef:true, unused:true, curly:true, browser:true, indent:4, maxerr:50 */
 
-if( !Function.prototype.curry ) {
-	Function.prototype.curry = function() {
-		if ( arguments.length < 1 ) {
+(function (global) {
+	'use strict';
+
+	var toArray;
+
+	if (typeof toArray !== 'function') {
+		toArray = function toArray(leEnum) {
+			return global.Array.prototype.slice.call(leEnum);
+		};
+	}
+
+	if (!global.Function.prototype.curry) {
+		Function.prototype.curry = function () {
+			if (arguments.length < 1) {
 				return this; //nothing to curry with - return function
-		}
-		var __method = this;
-		var args = toArray(arguments);
-		return function() {
+			}
+			var __method = this;
+			var args = toArray(arguments);
+			return function () {
 				return __method.apply(this, args.concat(toArray(arguments)));
-		}
+			};
+		};
 	}
-}
 
+})(window);
 
-(function( global, scope, undefined ) {
+(function (global, scope, undefined) {
+	'use strict';
   //Since we are populating the window scope you can build a custom plugin that populates some namespace.
-	var scope = scope || global,
+	scope = scope || global;
 	    //HTML elements as listed in MDN : https://developer.mozilla.org/en-US/docs/HTML/HTML5/HTML5_element_list.
-	    tags  = ["html",                                                                                      //Root element
-	            "head","title","base","link","meta","style",                                                  //Document metadata
-	            "script","noscript",                                                                          //Scripting
-	            "body","section","nav","article","aside",                                                     //Sections
-	            "h1","h2","h3","h4","h5","h6",                                                                //
-	            "hgroup","header","footer","address",                                                         //
-	            "p","hr","pre","blockquote","ol","ul","li","dl","dt","dd","figure","figcaption","div",        //Grouping content
-	            "a","em","strong","small","s","cite","q","dfn","abbr","data","time","code","var","samp","kbd",//Text-level semantics
-	            "sub","sup","i","b","u","mark","ruby","rt","rp","bdi","bdo","span","br","wbr",                //
-	            "ins","del",                                                                                  //Edits
-	            "img","iframe","embed","object","param","video","audio","source","track","canvas","map",      //Embedded content
-	            "area","svg","math",                                                                          //
-	            "table","caption","colgroup","col","tbody","thead","tfoot","tr","td","th",                    //Tabular data
-	            "form","fieldset","legend","label","input","button","select","datalist","optgroup","option",  //Forms
-	            "textarea","keygen","output","progress","meter",                                              //
-	            "details","summary","command","menu"],                                                        //Interactive elements
-	    //Returns a jQuery object wrapping the html object - see how this is faster than the jQuery way in the blog post.
-	    $h    = function( name ) { 
-		           return document.createElement( name ); 
-	           },
-	    i     = tags.length;
+	var tags = ["html",                                                                                                     //Root element
+	            "head", "title", "base", "link", "meta", "style",                                                           //Document metadata
+	            "script", "noscript",                                                                                       //Scripting
+	            "body", "section", "nav", "article", "aside",                                                               //Sections
+	            "h1", "h2", "h3", "h4", "h5", "h6",                                                                         //
+	            "hgroup", "header", "footer", "address",                                                                    //
+	            "p", "hr", "pre", "blockquote", "ol", "ul", "li", "dl", "dt", "dd", "figure", "figcaption", "div",          //Grouping content
+	            "a", "em", "strong", "small", "s", "cite", "q", "dfn", "abbr", "data", "time", "code", "var", "samp", "kbd",//Text-level semantics
+	            "sub", "sup", "i", "b", "u", "mark", "ruby", "rt", "rp", "bdi", "bdo", "span", "br", "wbr",                 //
+	            "ins", "del",                                                                                               //Edits
+	            "img", "iframe", "embed", "object", "param", "video", "audio", "source", "track", "canvas", "map",          //Embedded content
+	            "area", "svg", "math",                                                                                      //
+	            "table", "caption", "colgroup", "col", "tbody", "thead", "tfoot", "tr", "td", "th",                         //Tabular data
+	            "form", "fieldset", "legend", "label", "input", "button", "select", "datalist", "optgroup", "option",       //Forms
+	            "textarea", "keygen", "output", "progress", "meter",                                                        //
+	            "details", "summary", "command", "menu"],                                                                   //Interactive elements
+	    //Returns the Created Element
+	    $h   = function (name) { 
+	            return document.createElement(name); 
+	        },
+	    i    = tags.length;
 
-	while( i-- ) {
-		//Populates the scope with the Html handlers, as we are working with jQuery we prepend the '$' symbol for profit, I mean to follow the pattern.
-		scope['$'+tags[i]] = $h.curry(tags[i]);
+	while (i--) {
+		//Populates the scope with the Html handlers, we prepend the '$' symbol as some developers use it to indicate a DOM element.
+		scope['$' + tags[i]] = $h.curry(tags[i]);
 	}
 
-})( window, window );
-
+})(window, window);
