8c21981e6c24f862b7e2e4d2c9e2b40ab06a7c9e
Juan La Jara <juanlajara@gmail.com>
2013-01-30 16:52:33 -0500

    now tagnames are properties of scope( window ) and not functions

--- helm.js
+++ helm.js
@@ -1,81 +1,65 @@
-// Helm.js
-// 
-// Curry based DOM Factory Wrapper
-
-// usage: document.createElement('div') === $div()
-
-// visit http://b.juanlajara.com/ to know the status on the development of this plugin
-
-// @version: 0.0.1 Alpha-1
-// @author:  Juan La Jara http://juanlajara.com/
-// @date:    2012.12.10
-// @license: I guess it should be MIT
-
-
-// Add the Curry as learned from Angus Croll: http://javascriptweblog.wordpress.com/2010/04/05/curry-cooking-up-tastier-functions/
+//
+//
+//			Helm.js
+//			Curry based DOM Factory Wrapper
+//			https://github.com/wixo/helm.js
+//
+//			Usage: $div === document.createElement('div')
+//
+//			http://wxo.me/helm-blog to know the status on the development of this plugin
+//
+//			@version: 0.0.1 Alpha-2
+//			@author : Juan La Jara http://juanlajara.com/
+//			@date   : 2013.01.30
+//			@license: I guess it should be MIT
+//
+//
 
 /*jshint smarttabs:true, forin:true, noarg:true, noempty:true, eqeqeq:true, laxbreak:true, bitwise:true, strict:true, undef:true, unused:true, curly:true, browser:true, indent:4, maxerr:50 */
 
-(function (global) {
-  'use strict';
-
-  var toArray;
-
-  if (typeof toArray !== 'function') {
-    toArray = function toArray(leEnum) {
-      return global.Array.prototype.slice.call(leEnum);
-    };
-  }
-
-  if (!global.Function.prototype.curry) {
-    Function.prototype.curry = function () {
-      if (arguments.length < 1) {
-        return this; //nothing to curry with - return function
-      }
-      var __method = this;
-      var args = toArray(arguments);
-      return function () {
-        return __method.apply(this, args.concat(toArray(arguments)));
-      };
-    };
-  }
-
-})(window);
-
 (function (global, scope, undefined) {
-  'use strict';
+	'use strict';
+			
+	// Explicitly support a few good DOM libraries
+	var $    = global.jQuery || global.Zepto || global.ender || function (e) { return e; },
+	
+	// HTML elements as listed in MDN : http://wxo.me/html5-elements
+	    tags = ["html",	"head", "title", "base", "link", "meta", "style",
+	            "script", "noscript",	"body", "section", "nav", "article", 
+	            "aside", "h1", "h2", "h3", "h4", "h5", "h6",	"hgroup", 
+	            "header", "footer", "address", "p", "hr", "pre", "blockquote",
+	            "ol", "ul", "li", "dl", "dt", "dd", "figure", "figcaption", "div",
+	            "a", "em", "strong", "small", "s", "cite", "q", "dfn", "abbr",
+	            "data", "time", "code", "var", "samp", "kbd",	"sub", "sup", "i", 
+	            "b", "u", "mark", "ruby", "rt", "rp", "bdi", "bdo", "span", "br", 
+	            "wbr", "ins", "del",	"img", "iframe", "embed", "object", "param",
+	            "video", "audio", "source", "track", "canvas", "map",	"area", "svg",
+	            "math", "table", "caption", "colgroup", "col", "tbody", "thead", 
+	            "tfoot", "tr", "td", "th", "form", "fieldset", "legend", "label",
+	            "input", "button", "select", "datalist", "optgroup", "option", 
+	            "textarea", "keygen", "output", "progress", "meter", "details",
+	            "summary", "command", "menu"],
+	    i    = tags.length,
+	    defineProp;
 
-      //Explicitly support a few good DOM libraries
-  var $    = global.jQuery || global.Zepto || global.ender || function (e) { return e },
-      //HTML elements as listed in MDN : https://developer.mozilla.org/en-US/docs/HTML/HTML5/HTML5_element_list.
-      tags = ["html",                                                                                                     //Root element
-              "head", "title", "base", "link", "meta", "style",                                                           //Document metadata
-              "script", "noscript",                                                                                       //Scripting
-              "body", "section", "nav", "article", "aside",                                                               //Sections
-              "h1", "h2", "h3", "h4", "h5", "h6",                                                                         //
-              "hgroup", "header", "footer", "address",                                                                    //
-              "p", "hr", "pre", "blockquote", "ol", "ul", "li", "dl", "dt", "dd", "figure", "figcaption", "div",          //Grouping content
-              "a", "em", "strong", "small", "s", "cite", "q", "dfn", "abbr", "data", "time", "code", "var", "samp", "kbd",//Text-level semantics
-              "sub", "sup", "i", "b", "u", "mark", "ruby", "rt", "rp", "bdi", "bdo", "span", "br", "wbr",                 //
-              "ins", "del",                                                                                               //Edits
-              "img", "iframe", "embed", "object", "param", "video", "audio", "source", "track", "canvas", "map",          //Embedded content
-              "area", "svg", "math",                                                                                      //
-              "table", "caption", "colgroup", "col", "tbody", "thead", "tfoot", "tr", "td", "th",                         //Tabular data
-              "form", "fieldset", "legend", "label", "input", "button", "select", "datalist", "optgroup", "option",       //Forms
-              "textarea", "keygen", "output", "progress", "meter",                                                        //
-              "details", "summary", "command", "menu"],                                                                   //Interactive elements
-      //Returns the Created Element
-      $h   = function (name) { 
-              return $(document.createElement(name));
-          },
-      i    = tags.length;
+	// Use global if no scope was declared
+	scope = scope || global;
 
-  //Since we are populating the window scope you can build a custom plugin that populates some namespace.
-  scope = scope || global;
+	// Define the property in the scope, prepend $ to the tagname
+	defineProp = function (i) {
+		Object.defineProperty(scope, '$' + tags[i], {
+			get: function () {
+				return $(document.createElement(tags[i]));
+			},
+			set: function () {
+				throw new Error('Attempted to write to read-only property');
+			}
+		});
+	};
 
-  while (i--) {
-    //Populates the scope with the Html handlers, we prepend the '$' symbol as some developers use it to indicate a DOM element.
-    scope['$' + tags[i]] = $h.curry(tags[i]);
-  }
+	// Iterate through tagnames
+	while (i--) {
+		defineProp(i);
+	}
 
-})(window, window);
+})(window, this);
